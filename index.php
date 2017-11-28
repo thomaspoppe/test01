c380118c40887e3622315e3a85cc1a1cc2ebe2be
Thomas Poppe <tpoppe@pcextreme.nl>
2017-11-28 16:51:08 +0100

    Added basic folder structure and CSS files

--- index.php
+++ index.php
@@ -3,26 +3,21 @@
     <head>
         <title>Test 01</title>
 
-        <style>
-            body {
-                background-color: lightblue;
-            }
+        <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 
-            h1 {
-                color: #333;
-                text-align: center;
-            }
-
-            p {
-                font-family: verdana;
-                font-size: 20px;
-            }
-        </style>
     </head>
 
-    <body >
-        <h1>Text 01</h1>
-        <p>This is a test page.</p>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Text 01</h1>
+                    <p>This is a test page.</p>
+                </div>
+            </div>
+        </div>
     </body>
 
 </html>
