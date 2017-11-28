63ce8f30bd5f42bbe0c24f6fc856877b8b3a80f1
thomaspoppe <poppe.thomas@gmail.com>
2017-11-28 16:04:58 +0100

    Added index.html

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test 01</title>
+
+        <style>
+            body {
+                background-color: lightblue;
+            }
+
+            h1 {
+                color: white;
+                text-align: center;
+            }
+
+            p {
+                font-family: verdana;
+                font-size: 20px;
+            }
+        </style>
+    </head>
+
+    <body >
+        <h1>Text 01</h1>
+        <p>This is a test page.</p>
+    </body>
+
+</html>
